f336474daa210e86cbf76d918c71d513fd2242b8
gaby.santamaria@outlook.fr <gaby.santamaria@outlook.fr>
2021-02-05 21:52:38 +0100

    deleting useless css and fixing javascript

--- assets/js/gabyfle.js
+++ assets/js/gabyfle.js
@@ -1,23 +1,11 @@
 $(document).ready(function() {
-    /**
-     * Animating header stuff
-     * Principally text and colors
-     */
-    $("#header").hover(function(){
-        $("#big-title").html("$el_gabyflus <span class=\"code-red\">=</span>").addClass("code-white");
-        $("#description").html("<span class=\"code-yellow\">\"Fascinated by eye-popping pixels, badly written hieroglyphics, and air bubbles that are much too fresh.\"</span><span class=\"code-white\">;</span>").css("font-family", "Inconsolata");
-    }, function() {
-        $("#big-title").text("gabyfle").removeClass("code-white");
-        $("#description").html("Website of a guy fascinated by eye-popping pixels, badly written hieroglyphics, and air bubbles that are much too fresh.").css("font-family", "Open Sans");
-    });
-
-
     /*
      *  Get articles
      *  This is a dumb function but it does his job well so, it's alright.
      */
     $.get("https://blog.gabyfle.me/wp-json/wp/v2/posts", function(data) {
         if (data.length != 0) { $("#articles").removeClass("hide") }
+        else { return } // no articles
 
         let article = data[0];
 
